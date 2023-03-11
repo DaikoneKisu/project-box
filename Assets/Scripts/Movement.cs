db8e3c426eefa3b58d673fdb5f44a38fa70bb4f8
Daikone <eduardoarzolay@gmail.com>
2023-03-11 03:30:21 -0400

    Initializing repo

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float forceMagnitude;
+    Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rbody.AddForce(Vector2.right * forceMagnitude);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rbody.AddForce(Vector2.left * forceMagnitude);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rbody.AddTorque(forceMagnitude);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rbody.AddTorque(-forceMagnitude);
+        }
+    }
+}
